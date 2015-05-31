43476aaeb7dcce1954e10c0bd6c658cfe9506ade
Benedikt Wedenik <benedikt.wedenik@gmail.com>
2015-05-31 13:58:52 +0200

    autoformat netbeans

--- exercise3/src/ssd/JeopardyMoveHandler.java
+++ exercise3/src/ssd/JeopardyMoveHandler.java
@@ -18,55 +18,48 @@ import org.xml.sax.helpers.DefaultHandler;
  * TODO: Implement this content handler.
  */
 public class JeopardyMoveHandler extends DefaultHandler {
-	/**
-	 * Use this xPath variable to create xPath expression that can be
-	 * evaluated over the XML document.
-	 */
-	private static XPath xPath = XPathFactory.newInstance().newXPath();
-	
-	/**
-	 * Store and manipulate the Jeopardy XML document here.
-	 */
-	private Document jeopardyDoc;
-	
-	/**
-	 * This variable stores the text content of XML Elements.
-	 */
-	private String eleText;
 
-	//***TODO***
-	//Insert local variables here
-	
-    
-	
+    /**
+     * Use this xPath variable to create xPath expression that can be evaluated
+     * over the XML document.
+     */
+    private static XPath xPath = XPathFactory.newInstance().newXPath();
+
+    /**
+     * Store and manipulate the Jeopardy XML document here.
+     */
+    private Document jeopardyDoc;
+
+    /**
+     * This variable stores the text content of XML Elements.
+     */
+    private String eleText;
+
+    //***TODO***
+    //Insert local variables here
     public JeopardyMoveHandler(Document doc) {
-    	jeopardyDoc = doc;
+        jeopardyDoc = doc;
     }
-    
+
     @Override
     /**
      * SAX calls this method to pass in character data
      */
-  	public void characters(char[] text, int start, int length)
-  			    throws SAXException {
-  		eleText = new String(text, start, length);
-  	}
+    public void characters(char[] text, int start, int length)
+            throws SAXException {
+        eleText = new String(text, start, length);
+    }
 
     /**
-     * 
+     *
      * Return the current stored Jeopardy document
-     * 
+     *
      * @return XML Document
      */
-	public Document getDocument() {
-		return jeopardyDoc;
-	}
-    
-    //***TODO***
-	//Specify additional methods to parse the move document and modify the jeopardyDoc
-    
-    
-    
-	
-}
+    public Document getDocument() {
+        return jeopardyDoc;
+    }
 
+    //***TODO***
+    //Specify additional methods to parse the move document and modify the jeopardyDoc
+}
